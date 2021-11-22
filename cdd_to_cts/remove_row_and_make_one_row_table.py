d1a2b5ddd6b0edac0bfd2bb6da2a9bcf60b0fe84
Graham <gpoor@google.com>
2021-11-21 22:44:33 -0800

    Added new file to set up requirement processing.

---
+++ cdd_to_cts/remove_row_and_make_one_row_table.py
@@ -0,0 +1,5 @@
+#  Block to comment
+from table_ops import move_last_row_to_new_table
+
+if __name__ == '__main__':
+    move_last_row_to_new_table("/home/gpoor/PycharmProjects/parse-cdd-html-to-source/a_current_one/3.2.3.5-c3-up-to-c9.tsv")
