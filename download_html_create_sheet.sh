9b6b3a8def39fd580f403ad0eefa5b4758e49f02
Graham <gpoor@google.com>
2022-01-05 20:02:56 -0800

    Script file to build table.

---
+++ download_html_create_sheet.sh
@@ -0,0 +1,7 @@
+
+echo Downloads the cdd requirments from  https://source.android.com/compatibility/
+echo Enter the android version you want to generate a table of when prompted.
+cdd_version=n
+./download_requirments.sh
+
+python3 ./cdd_to_cts/parse_cdd_html.py
