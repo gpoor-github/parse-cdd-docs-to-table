b9699bd753e2df2285ea0b044428356c051d14e0
Graham <gpoor@google.com>
2022-01-12 13:58:19 -0800

    Added annotation processing.

---
+++ update_grep_of_cts_source.sh
@@ -0,0 +1,29 @@
+
+$ctsdir=$CTS_SOURCE_DIR
+echo Currently CTS source root dir is  ["$CTS_SOURCE_DIR"] running script to update environment variable
+python3 ./cdd_to_cts/static_data.py
+echo Now CTS source root dir is ["$CTS_SOURCE_DIR"]
+
+read -p "Enter the path the the CTS root  (n) for no Note: $CTS_SOURCE_DIR is the current dir, enter n to keep it " ctsdir
+if [[ $ctsdir != "n" ]]
+  then
+      CTS_SOURCE_DIR=$ctsdir
+      echo now the CTS source root dir is CTS_SOURCE_DIR ="$CTS_SOURCE_DIR"
+fi
+# android-cts-11.0_r5
+# #android-cts-12.0_r1
+# This script should be run in CTS source directory.
+echo this is what was entered $ctsdir
+#/home/gpoor/cts-source/cts
+#/home/gpoor/cts-12-source
+
+echo grep -inr "TestCases" --include \AndroidTest.xml "$ctsdir"/* > input_data_from_cts/testcases-modules.txt
+grep -inr "TestCases" --include \AndroidTest.xml "$ctsdir"/* > input_data_from_cts/testcases-modules.txt
+
+
+echo Will grep -inr -A 2 "@CddTest" --include \*.java "$ctsdir/*"
+
+grep -inr -A 2 "@CddTest" --include \*.java $ctsdir/* > input_data_from_cts/cdd_annotations_found.txt
+
+# Not used but good for additional information for mapping finds ccd in comments not java files etc.
+grep -inr -A 2 "[^.^/d]CDD " --include \*.* $ctsdir/* > input_data_from_cts/cdd_number.txt
